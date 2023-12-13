b7c599e05f38f91ca96796deed1982dc29da3a4c
Jack Tench <79285604+JackTench@users.noreply.github.com>
2023-12-13 17:41:34 +0000

    neovim config - init file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- Jack Tench 2023
+-- neovim config
+-- init.lua
+
+-- Load package manager.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load opts file.
+require("options")
+
+-- Load plugins file.
+require("lazy").setup("plugins")
